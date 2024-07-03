c3ce47e1a40fb7741eb4b2196c45b12ce2af4b7e
Gustavo Silva <gugassilva2000@gmail.com>
2024-07-03 01:23:24 -0300

    add first project

--- src/projectsData.tsx
+++ src/projectsData.tsx
@@ -1,12 +1,14 @@
+import igniteTimer from './assets/mock/ignite-timer-project.jpg'
+
 const ProjectsData = [
     {
         "id": 1,
-        "img": "",
-        "title": "Em breve",
-        "description": "Em breve",
-        "tool": "Em breve · Em breve",
-        "github": "",
-        "link": ""
+        "img": igniteTimer,
+        "title": "Ignite Timer",
+        "description": "Aplicação de pomodoro desenvolvida utilizando diversos conceitos importantes do ecossistema React como ContextAPI, useReducer, immer e muito mais.",
+        "tool": "React.js · TypeScript · Styled Components · React Hook Form · Zod · React Router Dom",
+        "github": "https://github.com/xgustavosz/ignite-timer",
+        "link": "https://ignite-timer-seven-neon.vercel.app/"
     },
     {
         "id": 2,
